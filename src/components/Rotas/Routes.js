1b3ff5bac8cc670e5aeed09e5013003ccf4ee95a
Victor <victorlima0302@gmail.com>
2023-11-19 01:40:10 -0300

    Add: Not found page

--- src/components/Rotas/Routes.js
+++ src/components/Rotas/Routes.js
@@ -5,6 +5,7 @@ import HomePage from '../../pages/HomePage';
 import LoginPage from '../../pages/LoginPage';
 import RegisterPage from '../../pages/RegisterPage';
 import ManageMusicPage from '../../pages/ManageMusicPage';
+import NotFound from '../../pages/NotFound';
 
 const AppRoutes = () => {
   return (
@@ -14,6 +15,7 @@ const AppRoutes = () => {
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/manage-music" element={<ManageMusicPage />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 };
---
+++ src/pages/NotFound.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const NotFoundWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  text-align: center;
+`;
+
+const ErrorMessage = styled.h1`
+  color: #ff0000; /* Cor vermelha */
+  margin-bottom: 10px;
+`;
+
+const NotFoundMessage = styled.p`
+  font-size: 18px;
+  margin-bottom: 20px;
+`;
+
+const HomeButton = styled(Link)`
+  padding: 10px 20px;
+  background-color: #037dfa; /* Cor azul */
+  color: #fff;
+  text-decoration: none;
+  border-radius: 4px;
+  cursor: pointer;
+`;
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  body {
+    font-family: 'Roboto', sans-serif;
+    background-color: #000000;
+    color: #fff;
+  }
+`;
+
+const NotFound = () => {
+    return (
+        <NotFoundWrapper>
+            <GlobalStyle />
+            <ErrorMessage>Erro 404</ErrorMessage>
+            <NotFoundMessage>Página não encontrada</NotFoundMessage>
+            <HomeButton to="/">Home</HomeButton>
+        </NotFoundWrapper>
+    );
+};
+
+export default NotFound;
